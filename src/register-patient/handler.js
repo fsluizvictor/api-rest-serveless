56917401cd5a2cbfd092909f6cab42205487e518
fsluizvictor <fs.luizvictor@gmail.com>
2021-10-11 22:59:45 -0300

    alteir function

--- src/register-patient/handler.js
+++ src/register-patient/handler.js
@@ -1,18 +1,19 @@
-'use strict';
+'use strict'
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const patients = [
+   { id: 1, nome: 'Maria', dataNascimento: '1984-11-01' },
+   { id: 2, nome: 'Joao', dataNascimento: '1980-01-16' },
+   { id: 3, nome: 'Jose', dataNascimento: '1998-06-06' }
+]
+module.exports.showPatients = async event => {
+ return {
+   statusCode: 200,
+   body: JSON.stringify(
+     {
+       patients
+     },
+     null,
+     2
+   )
+ }
+}
