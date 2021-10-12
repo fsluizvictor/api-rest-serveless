d02f1693a3efe5bdb2d52c9358397e1f9ba32d09
fsluizvictor <fs.luizvictor@gmail.com>
2021-10-12 20:56:44 -0300

    add connection and .json with data

--- src/register-patient/handler.js
+++ src/register-patient/handler.js
@@ -1,70 +1,66 @@
 'use strict'
 
 const patients = [
-   { id: 1, nome: 'Maria', dataNascimento: '1984-11-01' },
-   { id: 2, nome: 'Joao', dataNascimento: '1980-01-16' },
-   { id: 3, nome: 'Jose', dataNascimento: '1998-06-06' }
+  { id: 1, nome: 'Maria', dataNascimento: '1984-11-01' },
+  { id: 2, nome: 'Joao', dataNascimento: '1980-01-16' },
+  { id: 3, nome: 'Jose', dataNascimento: '1998-06-06' },
 ]
 
-const AWS = require("aws-sdk")
+const AWS = require('aws-sdk')
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
-const params = { 
-  TableName: "PATIENTS" 
+const params = {
+  TableName: 'PATIENTS',
 }
 
-
-module.exports.showPatients = async event => {
-
+module.exports.showPatients = async (event) => {
   try {
     let data = await dynamoDb.scan(params).promise()
-    
+
     return {
       statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (error) {
+    return {
+      statusCode: error.statusCode ? error.statusCode : 500,
       body: JSON.stringify(
         {
-          patients
+          error: error.name ? error.name : 'Exception',
+          message: error.message ? error.message : 'Unknown error',
         },
         null,
         2
-      )
+      ),
     }
- 
-  } catch (error) {
-    
   }
-
- 
 }
 
-module.exports.showPatient = async event => {
-  
-  const {
-    patientId
-  } = event.pathParameters
+module.exports.showPatient = async (event) => {
+  const { patientId } = event.pathParameters
 
-  const patient = patients.find((component) => component.id == patientId )
+  const patient = patients.find((component) => component.id == patientId)
 
-  if(!patient){
+  if (!patient) {
     return {
       statusCode: 404,
       body: JSON.stringify(
         {
-          error: "Not found"
+          error: 'Not found',
         },
         null,
         2
-      )
-    }  
+      ),
+    }
   }
-  
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        patient
+        patient,
       },
       null,
       2
-    )
+    ),
   }
- }
+}
